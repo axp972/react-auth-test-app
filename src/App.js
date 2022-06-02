9af2dd024e74f7a98625f824cd2e73ecadb6c80e
axp972 <ameya.pethe@sherwin.com>
2022-06-02 18:00:13 -0400

    Initial version of using out of the auzre app service authentication and deriving access/id token while using it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [idToken, setidToken] = useState(null);
+  const [someState, setSomeState] = useState(null);
+
+  useEffect(() => {
+    console.log("Hello! I am inside useEffect");
+    setSomeState("Some State Value!");
+
+    const hostUrl = window.location.protocol + "//" + window.location.host;
+
+    console.log("Current host- " + hostUrl);
+    console.log({ accessToken });
+
+    axios.get(hostUrl + "/.auth/me").then((response) => {
+      console.log(response.request.response);
+
+      let respBody = JSON.parse(response.request.response);
+      console.log(respBody[0]);
+      setAccessToken(respBody[0].access_token);
+      setidToken(respBody[0].id_token);
+    });
+
+    if ({ accessToken } == null) {
+      axios.get(hostUrl + "/.auth/me").then((response) => {
+        console.log(response);
+      });
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +49,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Access Token- {accessToken}</p>
+      <p>Id Token- {idToken}</p>
     </div>
   );
 }
